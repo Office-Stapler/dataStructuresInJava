bae0c297537418719362af15ffeb419506cb8326
Raymond Lam <z5255290@ad.unsw.edu.au>
2020-06-13 20:03:13 +1000

    Finished Stack data structure

---
+++ Stack.java
@@ -0,0 +1,50 @@
+package dataStructuresInJava;
+
+public class Stack<type> {
+    private LinkedList<type> list;
+    private int length;
+
+    /**
+     * Initialises an empty stack with elements of type <type>
+     */
+    public Stack() {
+        this.length = 0;
+        this.list = new LinkedList<type>();
+    }
+    /**
+     * Initialises a stack with an element of type <type>
+     * @parem an object of type <type> t to be the start of the queue.
+     */
+    public Stack(type t) {
+        this.length = 1;
+        this.list = new LinkedList<type>(t);
+    }
+    /**
+     * Gets the length of the stack
+     * @return the length of the stack.
+     */
+    public int getLength() {
+        return this.length;
+    }
+    /**
+     * Pushes an element into the stack.
+     * @parem a type t (type is the specified object for the generic class).
+     */
+    public void push(type val) {
+        this.list.insertAtTail(val);
+        this.length++;
+    }
+    /**
+     * Pops the stack and returns it.
+     * @return the lastest element added into the stack, this element is removed from
+     * the stack.
+     */
+    public ListNode<type> pop() {
+        ListNode<type> node = this.list.getAndRemoveTail();
+        if (node == null) return null;
+        this.length--;
+        return this.list.getAndRemoveTail();
+    }
+
+
+}
