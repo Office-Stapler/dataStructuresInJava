9351f53f5b749c365247739782bbb124e9f2e08b
Office-Stapler <raymond.lam@outlook.com.au>
2020-09-18 11:21:22 +1000

    Removed useless main

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import datastructures.BST;
-public class Main {
-    public static void main (String[] args) {
-        BST bst = new BST(5);
-        bst.insertValue(7);
-        System.out.println(bst.countNodes());
-    }
-}
